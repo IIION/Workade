a3a36dd4fc734eb51d6166784a85533ef8ebfeaa
Inho Choi <55151796+E-know@users.noreply.github.com>
2022-10-18 15:03:37 +0900

    Initial commit

---
+++ Workade/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+}
