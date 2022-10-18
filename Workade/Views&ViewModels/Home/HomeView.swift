5af55e03515aac31e06c24512b288f71cee4c644
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-18 23:34:59 +0900

    ✨ HomeVC의 네비게이션 바 구성
    추후 에반이 올릴 원본 이미지와 이름 겹치지않게 이미지이름 뒤에 Tamna 붙임
    withRenderingMode(.alwaysOriginal) 간편하게 쓰기위해 UIImage extension

--- Workade/Views&ViewModels/Home/HomeView.swift
+++ Workade/Views&ViewModels/Home/HomeView.swift
@@ -10,5 +10,28 @@ import UIKit
 class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        setupNavBar()
+    }
+}
+
+// MARK: UI setup 관련 Methods
+extension HomeViewController {
+    private func setupNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "WorkationerLogoTamna")?.original(),
+            style: .done,
+            target: nil,
+            action: nil
+        )
+        navigationItem.leftBarButtonItem?.isEnabled = false // no touch event
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "ProfileTamna")?.original(),
+            style: .done,
+            target: self,
+            action: nil // will connect to MyPageView
+        )
     }
 }
