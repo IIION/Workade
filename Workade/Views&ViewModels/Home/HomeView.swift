b3caff69e526c01fbf1c1b93199aa18ccee0bc96
Inho Choi <eknow25@kakao.com>
2022-10-18 15:30:14 +0900

    ✨ Add Example File

---
+++ Workade/Views&ViewModels/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/10/18.
+//
+
+import Foundation
---
+++ Workade/Views&ViewModels/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/10/18.
+//
+
+import Foundation
