29e7b3ef6acb1fdc7571b8b41a6a0560946ab66b
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-18 23:24:12 +0900

    ✨ SceneDelegate에서 HomeVC를 UINavigationController로 연결

--- Workade/Views&ViewModels/Home/HomeView.swift
+++ Workade/Views&ViewModels/Home/HomeView.swift
@@ -5,4 +5,10 @@
 //  Created by Inho Choi on 2022/10/18.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
