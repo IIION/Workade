11734c8023f11b69d49290d600fe3017264f9c24
eraser <eraser3031@gmail.com>
2022-10-19 17:30:15 +0900

    ✨ 색상 디자인 시스템 적용

--- Workade/Extentions/Color+.swift
+++ Workade/Extentions/Color+.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: Color
+// self.view.backgroundColor = .theme.primary
+
+extension UIColor {
+    static let theme = ColorTheme()
+    
+    static func rgb(_ rgbValue: Int) -> UIColor! {
+        return UIColor(
+            red: CGFloat((Float((rgbValue & 0xff0000) >> 16)) / 255.0),
+            green: CGFloat((Float((rgbValue & 0x00ff00) >> 8)) / 255.0),
+            blue: CGFloat((Float((rgbValue & 0x0000ff) >> 0)) / 255.0),
+            alpha: 1.0)
+    }
+}
+
+struct ColorTheme {
+    let primary = UIColor(named: "Primary")!
+    let secondary = UIColor(named: "Secondary")!
+    let tertiary = UIColor(named: "Tertiary")!
+    let quaternary = UIColor(named: "Quaternary")!
+    
+    let background = UIColor(named: "Background")!
+    let labelBackground = UIColor(named: "LabelBackground")!
+    let groupedBackground = UIColor(named: "GroupedBackground")!
+    let subGroupedBackground = UIColor(named: "SubGroupedBackground")!
+}
