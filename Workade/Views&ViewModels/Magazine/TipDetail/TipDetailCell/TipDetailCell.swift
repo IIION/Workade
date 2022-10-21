f893ac46bbe1efd37630d9fe23d3871f94b3b244
jeohong <jeohong59@gmail.com>
2022-10-21 17:41:20 +0900

    ✨ TipDetailCell UI 작업

---
+++ Workade/Views&ViewModels/Magazine/TipDetail/TipDetailCell/TipDetailCell.swift
@@ -0,0 +1,95 @@
+//
+//  TipDetailCell.swift
+//  Workade
+//
+//  Created by Hong jeongmin on 2022/10/21.
+//
+
+import UIKit
+
+class TipDetailCell: UICollectionViewCell {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .theme.background
+        label.font = .customFont(for: .headline)
+        // Temp
+        label.text = "내 성격에 맞는\n장소 찾는 법"
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let bookMarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "bookmark"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .theme.background
+        
+        return button
+    }()
+    
+    private let cellBackgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        // Temp
+        imageView.image = UIImage(named: "SampleTipImage") ?? UIImage()
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 13.2
+        
+        self.setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func setupLayout() {
+        contentView.addSubview(cellBackgroundImage)
+        NSLayoutConstraint.activate([
+            cellBackgroundImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            cellBackgroundImage.topAnchor.constraint(equalTo: self.topAnchor),
+            cellBackgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            cellBackgroundImage.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+        
+        contentView.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20)
+        ])
+        
+        contentView.addSubview(bookMarkButton)
+        NSLayoutConstraint.activate([
+            bookMarkButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            bookMarkButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 16)
+        ])
+    }
+}
+
+import SwiftUI
+
+struct TipDetailCellRepresentable: UIViewRepresentable {
+    typealias UIViewType = TipDetailCell
+    
+    func makeUIView(context: Context) -> TipDetailCell {
+        return TipDetailCell()
+    }
+    
+    func updateUIView(_ uiView: TipDetailCell, context: Context) {}
+}
+
+@available(iOS 13.0.0, *)
+struct TipDetailCelllPreview: PreviewProvider {
+    static var previews: some View {
+        TipDetailCellRepresentable()
+            .ignoresSafeArea()
+            .frame(width: 165, height: 165)
+            .previewLayout(.sizeThatFits)
+    }
+}
