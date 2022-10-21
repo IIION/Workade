89a00c2bb963e8152b610a7ba5d73600d8dc61ea
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-21 21:21:10 +0900

    ✨ systemImage Configuration을 매번 작성하지않도록 UIImage 확장, 편의 이니셜라이저 생성

--- Workade/Extentions/UIImage+.swift
+++ Workade/Extentions/UIImage+.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 extension UIImage {
+    convenience init?(symbolName: String, font: UIFont, color: UIColor = .black) {
+        let configuration = UIImage.SymbolConfiguration(font: font)
+        self.init(systemName: symbolName, withConfiguration: configuration)
+        self.withTintColor(color)
+    }
+    
     func setOriginal() -> UIImage {
         return self.withRenderingMode(.alwaysOriginal)
     }
