4d0c5557088a82c3089f0122d97fc0061afbef9f
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-22 23:05:06 +0900

    ✨ Magazine 모델 추가

--- Workade/Models/HomeModel.swift
+++ Workade/Models/HomeModel.swift
@@ -14,3 +14,11 @@ struct Office {
     let latitude: Double
     let longitude: Double
 }
+
+// 식별자로 북마크 여부 판단 및 마이페이지에서 찜한 애들만 불러옴
+// Int는 아니어도 무관 (고유한 값이기만 하면 됩니다.)
+struct Megazine {
+    let id: Int
+    let title: String
+    let profileImage: UIImage
+}
