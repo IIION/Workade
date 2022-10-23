2de9c3a32e2e1b82a03c29087ad6e5972419828b
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-24 01:58:06 +0900

    ✏️ 프로퍼티 확인용으로 써놨던 코드 제거

--- Workade/SceneDelegate.swift
+++ Workade/SceneDelegate.swift
@@ -17,7 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        windowScene.statusBarManager
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
