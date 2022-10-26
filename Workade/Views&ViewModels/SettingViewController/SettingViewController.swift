e19a85ed8bcb7a9276cbc7654c6262663ab79f1e
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-26 18:27:00 +0900

    ✨ 설정뷰컨트롤러 초기 구성 완료

---
+++ Workade/Views&ViewModels/SettingViewController/SettingViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SettingViewController.swift
+//  Workade
+//
+//  Created by Hyeonsoo Kim on 2022/10/26.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    private let titleView = TitleView(title: "설정")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupLayout()
+    }
+    
+    @objc
+    func popToMyPageView() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: UI setup 관련 Methods
+extension SettingViewController {
+    func setupNavigationBar() {
+        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: SFSymbol.chevronLeft.image,
+            style: .done,
+            target: self,
+            action: #selector(popToMyPageView)
+        )
+    }
+    
+    func setupLayout() {
+        view.addSubview(titleView)
+        
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+    }
+}
