cebb53965faa8950fd332e885b8de9bcbf160c48
ryu <munam4@naver.com>
2022-10-26 17:27:37 +0900

    ✨ 지역 이름, 워크스페이스 이름, 맵 버튼 레이아웃 추가, 디테일 정보 보여줄 부분 스크롤 뷰 추가

--- Workade/Views&ViewModels/NearbyPlace/NearbyPlaceView.swift
+++ Workade/Views&ViewModels/NearbyPlace/NearbyPlaceView.swift
@@ -43,6 +43,55 @@ class NearbyPlaceView: UIView {
         return imageView
     }()
     
+    let locationLabel: UILabel = {
+        let locationLabel = UILabel()
+        locationLabel.text = "제주도"
+        locationLabel.font = UIFont.customFont(for: .title3)
+        locationLabel.textColor = .white
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return locationLabel
+    }()
+    
+    let placeLabel: UILabel = {
+        let placeLabel = UILabel()
+        placeLabel.text = "O-Peace"
+        // TODO: AccentTitle1 폰트와 background_Light 컬러가 없습니다. 추후 변경할 예정입니다.
+        placeLabel.font = UIFont.customFont(for: .title1)
+        placeLabel.textColor = .white
+        placeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return placeLabel
+    }()
+    
+    lazy var mapButton: UIButton = {
+        let mapButton = UIButton()
+        let blur = UIVisualEffectView(effect:
+                                        UIBlurEffect(style: UIBlurEffect.Style.light))
+        mapButton.frame.size = CGSize(width: 48, height: 48)
+        mapButton.layer.cornerRadius = mapButton.bounds.height / 2
+        
+        let configuration = UIImage.SymbolConfiguration(font: .systemFont(ofSize: 17, weight: .semibold))
+        var image = UIImage(systemName: "map", withConfiguration: configuration)
+        image = image?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        mapButton.setImage(image, for: .normal)
+        
+        blur.frame = mapButton.bounds
+        blur.isUserInteractionEnabled = false
+        blur.layer.cornerRadius = 0.5 * mapButton.bounds.size.height
+        blur.clipsToBounds = true
+        mapButton.insertSubview(blur, belowSubview: mapButton.imageView!)
+        
+        return mapButton
+    }()
+    
+    let mapButtonContainer: UIView = {
+        let mapButtonContainer = UIView(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
+        mapButtonContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        return mapButtonContainer
+    }()
+    
     private lazy var segmentedControl: UISegmentedControl = {
         let segmentedControl = CustomSegmentedControl(items: ["소개", "갤러리"])
         segmentedControl.setTitleTextAttributes([
@@ -68,6 +117,23 @@ class NearbyPlaceView: UIView {
         return segmentUnderLine
     }()
     
+    let detailScrollView: UIScrollView = {
+        let detailScrollView = UIScrollView()
+        detailScrollView.alwaysBounceVertical = true
+        detailScrollView.isScrollEnabled = false
+        detailScrollView.showsVerticalScrollIndicator = false
+        detailScrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return detailScrollView
+    }()
+    
+    let detailContensContainer: UIView = {
+        let detailContensContainer = UIView()
+        detailContensContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        return detailContensContainer
+    }()
+    
     private let introduceView: IntroduceView = {
         let view = IntroduceView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -87,8 +153,8 @@ class NearbyPlaceView: UIView {
         super.init(frame: frame)
         
         // 스크롤 뷰의 영역을 컨텐츠 크기에 따라 dynamic하게 변경하기 위한 설정
-        introduceBottomConstraints = introduceView.bottomAnchor.constraint(equalTo: contentsContainer.bottomAnchor, constant: -20)
-        galleryBottomConstraints = galleryView.bottomAnchor.constraint(equalTo: contentsContainer.bottomAnchor, constant: -20)
+        introduceBottomConstraints = introduceView.bottomAnchor.constraint(equalTo: detailContensContainer.bottomAnchor, constant: -20)
+        galleryBottomConstraints = galleryView.bottomAnchor.constraint(equalTo: detailContensContainer.bottomAnchor, constant: -20)
         
         setupLayout()
     }
@@ -99,18 +165,12 @@ class NearbyPlaceView: UIView {
     
     private func setupLayout() {
         setupScrollViewLayout()
+        setupPlaceInfoLayout()
         setupNearbyPlaceDetailLayout()
     }
     
     private func setupScrollViewLayout() {
         addSubview(scrollView)
-        let scrollViewGuide = scrollView.contentLayoutGuide
-        
-        // priority 설정을 통해,
-        let placeImageViewTopConstraint = placeImageView.topAnchor.constraint(equalTo: topAnchor)
-        // layout이 깨지는 것을 방지하기 위한 우선순위 설정
-        placeImageViewTopConstraint.priority = .defaultHigh
-        
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: topAnchor),
             scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
@@ -118,6 +178,7 @@ class NearbyPlaceView: UIView {
             scrollView.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
         
+        let scrollViewGuide = scrollView.contentLayoutGuide
         scrollView.addSubview(contentsContainer)
         NSLayoutConstraint.activate([
             contentsContainer.topAnchor.constraint(equalTo: scrollViewGuide.topAnchor),
@@ -126,7 +187,7 @@ class NearbyPlaceView: UIView {
             contentsContainer.trailingAnchor.constraint(equalTo: scrollViewGuide.trailingAnchor),
             contentsContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
-        
+
         contentsContainer.addSubview(placeImageContainer)
         NSLayoutConstraint.activate([
             placeImageContainer.topAnchor.constraint(equalTo: contentsContainer.topAnchor),
@@ -135,6 +196,9 @@ class NearbyPlaceView: UIView {
             placeImageContainer.heightAnchor.constraint(equalToConstant: 420)
         ])
         
+        let placeImageViewTopConstraint = placeImageView.topAnchor.constraint(equalTo: topAnchor)
+        // layout이 깨지는 것을 방지하기 위한 우선순위 설정
+        placeImageViewTopConstraint.priority = .defaultHigh
         contentsContainer.addSubview(placeImageView)
         NSLayoutConstraint.activate([
             placeImageViewTopConstraint,
@@ -144,9 +208,7 @@ class NearbyPlaceView: UIView {
             // heightAnchor 설정을 통해, 세로가 긴 이미지가 들어올때 이미지 영역 깨지는걸 방지.
             placeImageView.heightAnchor.constraint(greaterThanOrEqualTo: placeImageContainer.heightAnchor)
         ])
-    }
-    
-    private func setupNearbyPlaceDetailLayout() {
+        
         contentsContainer.addSubview(segmentedControl)
         NSLayoutConstraint.activate([
             segmentedControl.topAnchor.constraint(equalTo: placeImageContainer.bottomAnchor),
@@ -163,31 +225,90 @@ class NearbyPlaceView: UIView {
             segmentUnderLine.heightAnchor.constraint(equalToConstant: 2)
         ])
         
-        contentsContainer.addSubview(introduceView)
+    }
+    
+    private func setupPlaceInfoLayout() {
+        contentsContainer.addSubview(placeLabel)
         NSLayoutConstraint.activate([
-            introduceView.topAnchor.constraint(equalTo: segmentUnderLine.bottomAnchor, constant: 20),
-            introduceView.leadingAnchor.constraint(equalTo: contentsContainer.leadingAnchor, constant: 20),
-            introduceView.trailingAnchor.constraint(equalTo: contentsContainer.trailingAnchor, constant: -20),
+            placeLabel.bottomAnchor.constraint(equalTo: placeImageContainer.bottomAnchor, constant: -20),
+            placeLabel.leadingAnchor.constraint(equalTo: placeImageContainer.leadingAnchor, constant: 20)
+        ])
+        
+        contentsContainer.addSubview(locationLabel)
+        NSLayoutConstraint.activate([
+            locationLabel.bottomAnchor.constraint(equalTo: placeLabel.topAnchor, constant: -5),
+            locationLabel.leadingAnchor.constraint(equalTo: placeImageContainer.leadingAnchor, constant: 20)
+        ])
+        
+        contentsContainer.addSubview(mapButtonContainer)
+        NSLayoutConstraint.activate([
+            mapButtonContainer.trailingAnchor.constraint(equalTo: placeImageContainer.trailingAnchor, constant: -17),
+            mapButtonContainer.bottomAnchor.constraint(equalTo: placeImageContainer.bottomAnchor, constant: -17)
+        ])
+        
+        mapButtonContainer.addSubview(mapButton)
+        NSLayoutConstraint.activate([
+            mapButton.centerXAnchor.constraint(equalTo: mapButtonContainer.centerXAnchor),
+            mapButton.centerYAnchor.constraint(equalTo: mapButtonContainer.centerYAnchor)
+        ])
+    }
+    
+    private func setupNearbyPlaceDetailLayout() {
+        contentsContainer.addSubview(detailScrollView)
+        detailScrollView.addSubview(detailContensContainer)
+        NSLayoutConstraint.activate([
+            detailScrollView.topAnchor.constraint(equalTo: segmentUnderLine.bottomAnchor),
+            detailScrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            detailScrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            detailScrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentsContainer.bottomAnchor.constraint(equalTo: detailContensContainer.bottomAnchor, constant: 420)
+        ])
+        
+        let detailScrollViewGuide = detailScrollView.contentLayoutGuide
+        NSLayoutConstraint.activate([
+            detailContensContainer.topAnchor.constraint(equalTo: detailScrollViewGuide.topAnchor),
+            detailContensContainer.bottomAnchor.constraint(equalTo: detailScrollViewGuide.bottomAnchor),
+            detailContensContainer.leadingAnchor.constraint(equalTo: detailScrollViewGuide.leadingAnchor),
+            detailContensContainer.trailingAnchor.constraint(equalTo: detailScrollViewGuide.trailingAnchor),
+            detailContensContainer.widthAnchor.constraint(equalTo: detailScrollView.widthAnchor)
+        ])
+        
+        detailContensContainer.addSubview(introduceView)
+        NSLayoutConstraint.activate([
+            introduceView.topAnchor.constraint(equalTo: detailContensContainer.topAnchor, constant: 20),
+            introduceView.leadingAnchor.constraint(equalTo: detailContensContainer.leadingAnchor, constant: 20),
+            introduceView.trailingAnchor.constraint(equalTo: detailContensContainer.trailingAnchor, constant: -20),
             introduceBottomConstraints
         ])
         
-        contentsContainer.addSubview(galleryView)
+        detailContensContainer.addSubview(galleryView)
         NSLayoutConstraint.activate([
-            galleryView.topAnchor.constraint(equalTo: segmentUnderLine.bottomAnchor, constant: 20),
-            galleryView.leadingAnchor.constraint(equalTo: contentsContainer.leadingAnchor, constant: 20),
-            galleryView.trailingAnchor.constraint(equalTo: contentsContainer.trailingAnchor, constant: -20)
+            galleryView.topAnchor.constraint(equalTo: detailContensContainer.topAnchor, constant: 20),
+            galleryView.leadingAnchor.constraint(equalTo: detailContensContainer.leadingAnchor, constant: 20),
+            galleryView.trailingAnchor.constraint(equalTo: detailContensContainer.trailingAnchor, constant: -20)
         ])
     }
     
     @objc
     private func indexChanged(_ segmentedControl: UISegmentedControl) {
+        let safeTop = safeAreaInsets.top
         switch segmentedControl.selectedSegmentIndex {
         case 0:
+            detailScrollView.isScrollEnabled = true
+            detailScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+            scrollView.isScrollEnabled = true
             introduceView.isHidden = false
             galleryView.isHidden = true
             galleryBottomConstraints.isActive = false
             introduceBottomConstraints.isActive = true
         case 1:
+            // 갤러리 누르면 세그먼트 위치로 스크롤 이동.
+            detailScrollView.isScrollEnabled = true
+            detailScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+            // 전체 뷰의 스크롤은 멈춰야함.
+            scrollView.isScrollEnabled = false
+            // 전체뷰의 스크롤 위치를 이미지가 끝나는 지점으로 맞춰줘야함
+            scrollView.setContentOffset(CGPoint(x: 0, y: 420 - safeTop), animated: false)
             introduceView.isHidden = true
             galleryView.isHidden = false
             introduceBottomConstraints.isActive = false
