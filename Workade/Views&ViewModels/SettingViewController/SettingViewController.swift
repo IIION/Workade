73c549fcd83d9022b6f0620d00654dc701b2e8b7
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-26 18:35:35 +0900

    ✨ 설정 뷰컨트롤러 구성 완료

--- Workade/Views&ViewModels/SettingViewController/SettingViewController.swift
+++ Workade/Views&ViewModels/SettingViewController/SettingViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class SettingViewController: UIViewController {
     private let titleView = TitleView(title: "설정")
+    
+    private let infoButton: NavigateButton = {
+        let button = NavigateButton(image: SFSymbol.info.image, text: "서비스 정보")
+        button.layer.backgroundColor = UIColor.systemGroupedBackground.cgColor
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .theme.background
         
         setupNavigationBar()
         setupLayout()
@@ -37,10 +46,18 @@ extension SettingViewController {
     
     func setupLayout() {
         view.addSubview(titleView)
+        view.addSubview(infoButton)
         
         NSLayoutConstraint.activate([
             titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            infoButton.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 30),
+            infoButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            infoButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            infoButton.heightAnchor.constraint(equalToConstant: 74)
         ])
     }
 }
