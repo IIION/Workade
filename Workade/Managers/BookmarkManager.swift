6e05596744fc98934739d77b9fc100d63bd9fc20
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-28 02:35:45 +0900

    ✨ 북마크 매니저 생성 - 북마크 변화 감지 역할

---
+++ Workade/Managers/BookmarkManager.swift
@@ -0,0 +1,14 @@
+//
+//  BookmarkManager.swift
+//  Workade
+//
+//  Created by Hyeonsoo Kim on 2022/10/28.
+//
+
+import Foundation
+
+final class BookmarkManager {
+    static let shared = BookmarkManager()
+    
+    var clickedMagazineId = MultiBinder("")
+}
