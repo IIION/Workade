44513b00faf0e084d5f8238689d9ea5cba6edfd6
jeohong <jeohong59@gmail.com>
2022-11-15 21:28:57 +0900

    ♻️ 갤러리 사진 불러오기 구현 ( 전체 스크롤 미완성 )

--- Workade/Views&ViewModels/NearbyPlace/NearbyPlaceViewController.swift
+++ Workade/Views&ViewModels/NearbyPlace/NearbyPlaceViewController.swift
@@ -34,7 +34,7 @@ class NearbyPlaceViewController: UIViewController {
     private var defaultScrollYOffset: CGFloat = 0
     
     // Gallery 관련 프로퍼티
-    //        let transitionManager = CardTransitionMananger()
+    let transitionManager = CardTransitionMananger()
     var columnSpacing: CGFloat = 20
     var isLoading: Bool = false
     
@@ -76,6 +76,12 @@ class NearbyPlaceViewController: UIViewController {
         view.backgroundColor = .theme.background
         nearbyPlaceImageView.delegate = self
         
+        // GalleryView 델리게이트 위임
+        nearbyPlaceDetailView.galleryView.collectionView.dataSource = self
+        nearbyPlaceDetailView.galleryView.collectionView.delegate = self
+        nearbyPlaceDetailView.galleryView.layout.delegate = self
+        
+        setupGalleryView()
         setupLayout()
         setupCustomNavigationBar()
     }
@@ -163,6 +169,15 @@ class NearbyPlaceViewController: UIViewController {
             return
         }
     }
+    
+    /// 갤러리 사진들 불러오는 함수
+    private func setupGalleryView() {
+        Task {
+            guard let url = URL(string: office.galleryURL) else { return }
+            await galleryViewModel.fetchContent(by: url)
+            nearbyPlaceDetailView.galleryView.collectionView.reloadData()
+        }
+    }
 }
 
 extension NearbyPlaceViewController: InnerTouchPresentDelegate {
@@ -172,204 +187,49 @@ extension NearbyPlaceViewController: InnerTouchPresentDelegate {
         present(viewController, animated: true)
     }
 }
-
-//        view.backgroundColor = .white
-//        nearbyPlaceView.scrollView.delegate = self
-//        nearbyPlaceView.delegate = self
-//        nearbyPlaceView.detailScrollView.delegate = self
-//        navigationController?.isNavigationBarHidden = true
-//
-//        // GalleryView 델리게이트 위임
-//        nearbyPlaceView.galleryView.collectionView.dataSource = self
-//        nearbyPlaceView.galleryView.collectionView.delegate = self
-//        nearbyPlaceView.galleryView.layout.delegate = self
-//
-//        setupNearbyPlaceView()
-//        setupCustomNavigationBar()
-//        setupGalleryView()
-//        setupIntroduceView()
-//    }
-//
-//    private func setupGalleryView() {
-//        Task {
-//            guard let url = URL(string: office.galleryURL) else { return }
-//            await galleryViewModel.fetchContent(by: url)
-//            nearbyPlaceView.galleryView.collectionView.reloadData()
-//        }
-//    }
-//
-//    private func setupIntroduceView() {
-//        Task {
-//            await introduceViewModel.fetchData()
-//        }
-//        introduceViewModel.introductions.bind { contents in
-//            for content in contents {
-//                switch content.type {
-//                case "Text":
-//                    let label = UILabel()
-//                    label.text = content.context
-//                    if let font = content.font {
-//                        label.font = .customFont(for: CustomTextStyle(rawValue: font) ?? .articleBody)
-//                    }
-//                    if let color = content.color {
-//                        label.textColor = UIColor(named: color)
-//                    }
-//                    label.lineBreakMode = .byWordWrapping
-//                    label.numberOfLines = 0
-//                    label.setLineHeight(lineHeight: 12.0)
-//                    self.nearbyPlaceView.introduceView.stackView.addArrangedSubview(label)
-//                case "Image":
-//                    let imageView = UIImageView()
-//                    let imageURL = content.context
-//                    imageView.translatesAutoresizingMaskIntoConstraints = false
-//                    Task {
-//                        let image = await self.introduceViewModel.fetchImage(urlString: imageURL)
-//                        imageView.image = image
-//                        let width = image.size.width
-//                        let height = image.size.height
-//
-//                        imageView.contentMode = .scaleToFill
-//                        imageView.layer.cornerRadius = 20
-//                        imageView.clipsToBounds = true
-//
-//                        imageView.heightAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: height/width).isActive = true
-//                    }
-//                    self.nearbyPlaceView.introduceView.stackView.addArrangedSubview(imageView)
-//                default:
-//                    break
-//                }
-//            }
-//        }
-//    }
-//
-//    private func setupNearbyPlaceView() {
-//        view.addSubview(nearbyPlaceView)
-//        NSLayoutConstraint.activate([
-//            nearbyPlaceView.topAnchor.constraint(equalTo: view.topAnchor),
-//            nearbyPlaceView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//            nearbyPlaceView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            nearbyPlaceView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-//        ])
-//    }
-//}
-//extension NearbyPlaceViewController: UIScrollViewDelegate {
-//    @objc
-//    func clickedCloseButton(sender: UIButton) {
-//        self.dismiss(animated: true, completion: nil)
-//    }
-//
-//    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-//        let totalOffset = scrollView.contentOffset.y
-//        if totalOffset < -.topSafeArea {
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//    }
-//
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        let totalOffset = scrollView.contentOffset.y
-//        let detailOffset = nearbyPlaceView.detailScrollView.contentOffset.y
-//
-//        switch scrollView {
-//        case nearbyPlaceView.scrollView:
-//            if totalOffset > 0 {
-//                if totalOffset > 315 {
-//                    scrollView.setContentOffset(CGPoint(x: 0, y: 315), animated: false)
-//                    // 전체 스크롤 뷰를 막고, 디테일 뷰의 스크롤 뷰를 활성화 시킴.
-//                    nearbyPlaceView.scrollView.isScrollEnabled = false
-//                    nearbyPlaceView.detailScrollView.isScrollEnabled = true
-//                } else {
-//                    if nearbyPlaceView.segmentedControl.selectedSegmentIndex == 0 {
-//                        nearbyPlaceView.detailScrollView.isScrollEnabled = false
-//                    }
-//                    customNavigationBar.view.alpha = totalOffset / (.topSafeArea + 259)
-//                    nearbyPlaceView.placeImageView.alpha = 1 - (totalOffset / (.topSafeArea + 259))
-//                }
-//            } else {
-//                customNavigationBar.view.alpha = 0
-//                nearbyPlaceView.placeImageView.alpha = 1
-//            }
-//        case nearbyPlaceView.detailScrollView:
-//            if detailOffset <= 0 {
-//                nearbyPlaceView.detailScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
-//                if nearbyPlaceView.segmentedControl.selectedSegmentIndex == 0 {
-//                    nearbyPlaceView.scrollView.isScrollEnabled = true
-//                }
-//            }
-//        default:
-//            break
-//        }
-//    }
-//}
-//
-//// GalleryView(collectionView 델리게이트 익스텐션)
-//extension NearbyPlaceViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return galleryViewModel.images.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let identifier = GalleryCollectionViewCell.identifier
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? GalleryCollectionViewCell
-//        cell?.imageView.image = galleryViewModel.images[indexPath.row]
-//        guard let cell = cell else { fatalError() }
-//
-//        return cell
-//    }
-//}
-//
-//extension NearbyPlaceViewController: UICollectionViewDelegate {
-//    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-//        let image = galleryViewModel.images[indexPath.row]
-//        let viewController = GalleryDetailViewController()
-//        viewController.image = image
-//        viewController.modalPresentationStyle = .overCurrentContext
-//        viewController.transitioningDelegate = transitionManager
-//        self.present(viewController, animated: true)
-//
-//        return true
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-//        if indexPath.row == galleryViewModel.images.count - 1, galleryViewModel.isCanLoaded {
-//            Task { [weak self] in
-//                await self?.galleryViewModel.fetchImages()
-//                self?.nearbyPlaceView.galleryView.collectionView.reloadData()
-//            }
-//        }
-//    }
-//}
-//
-//extension NearbyPlaceViewController: TwoLineLayoutDelegate {
-//    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
-//        let image = galleryViewModel.images[indexPath.row]
-//        let aspectR = image.size.width / image.size.height
-//
-//        return (collectionView.frame.width - columnSpacing * 3) / 2 * 1 / aspectR
-//    }
-//}
-
-import SwiftUI
-
-struct NearbyPlaceViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = NearbyPlaceViewController
-    
-    func makeUIViewController(context: Context) -> NearbyPlaceViewController {
-        return NearbyPlaceViewController(office: Office(officeName: "제주 O-PEACE",
-                                                        regionName: "제주도",
-                                                        imageURL: "https://raw.githubusercontent.com/IIION/WorkadeData/main/Home/Image/Office/opiece.jpeg",
-                                                        introduceURL: "https://raw.githubusercontent.com/IIION/WorkadeData/main/Office/opiece.json",
-                                                        galleryURL: "https://raw.githubusercontent.com/IIION/WorkadeData/main/Office/opiecegallery.json",
-                                                        latitude: 33.5328984,
-                                                        longitude: 126.6311401,
-                                                        spots: []))
+// GalleryView(collectionView 델리게이트 익스텐션)
+extension NearbyPlaceViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return galleryViewModel.images.count
     }
     
-    func updateUIViewController(_ uiViewController: NearbyPlaceViewController, context: Context) {}
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = GalleryCollectionViewCell.identifier
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? GalleryCollectionViewCell
+        cell?.imageView.image = galleryViewModel.images[indexPath.row]
+        guard let cell = cell else { fatalError() }
+        
+        return cell
+    }
 }
 
-@available(iOS 13.0.0, *)
-struct NearbyPlaceViewControllerPreview: PreviewProvider {
-    static var previews: some View {
-        NearbyPlaceViewControllerRepresentable()
+extension NearbyPlaceViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        let image = galleryViewModel.images[indexPath.row]
+        let viewController = GalleryDetailViewController()
+        viewController.image = image
+        viewController.modalPresentationStyle = .overCurrentContext
+        viewController.transitioningDelegate = transitionManager
+        self.present(viewController, animated: true)
+        
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == galleryViewModel.images.count - 1, galleryViewModel.isCanLoaded {
+            Task { [weak self] in
+                await self?.galleryViewModel.fetchImages()
+                self?.nearbyPlaceDetailView.galleryView.collectionView.reloadData()
+            }
+        }
+    }
+}
+
+extension NearbyPlaceViewController: TwoLineLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        let image = galleryViewModel.images[indexPath.row]
+        let aspectR = image.size.width / image.size.height
+        
+        return (collectionView.frame.width - columnSpacing * 3) / 2 * 1 / aspectR
     }
 }
