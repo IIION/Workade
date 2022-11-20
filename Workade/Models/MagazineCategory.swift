652e2972625fd78e1c9b0e17e02e6a1755d93242
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-11-21 04:24:32 +0900

    ✨ MagazineCategory 열거형 정의

---
+++ Workade/Models/MagazineCategory.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineCategory.swift
+//  Workade
+//
+//  Created by Hyeonsoo Kim on 2022/11/21.
+//
+
+import Foundation
+
+enum MagazineCategory {
+    case total
+    case tip
+    case column
+    case review
+    case wishList
+}
