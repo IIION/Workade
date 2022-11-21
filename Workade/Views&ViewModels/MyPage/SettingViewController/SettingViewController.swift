22185a851883eb78be0694e1dbbf622407329861
jeohong <jeohong59@gmail.com>
2022-11-21 22:55:23 +0900

    ✨ SettingViewController UI 구현

--- Workade/Views&ViewModels/MyPage/SettingViewController/SettingViewController.swift
+++ Workade/Views&ViewModels/MyPage/SettingViewController/SettingViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 final class SettingViewController: UIViewController {
+    // 현재 앱의 버전 정보 + 빌드 정보 불러오기
+    let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+    let bundleVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
+        
     private let loginInformationView: LoginInformationView = {
         let logInInformationView = LoginInformationView()
         logInInformationView.translatesAutoresizingMaskIntoConstraints = false
@@ -49,6 +53,39 @@ final class SettingViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
+    }()
+    
+    private let withdrawalButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원탈퇴", for: .normal)
+        button.titleLabel?.font = .customFont(for: .caption2)
+        button.setTitleColor(.theme.tertiary, for: .normal)
+        button.backgroundColor = .theme.background
+        button.layer.cornerRadius = 10
+        
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.theme.groupedBackground.cgColor
+        
+        button.addAction(UIAction(handler: { _ in
+            // TODO: 회원탈퇴 로직
+            print("회원탈퇴 버튼 클릭")
+        }), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private lazy var versionInformationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "developed by iLiON\nVer \((version) + "." + (bundleVersion))"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .customFont(for: .caption2)
+        label.textColor = .theme.tertiary
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
     }()
     
     override func viewDidLoad() {
@@ -105,6 +142,20 @@ private extension SettingViewController {
             licenseButton.leadingAnchor.constraint(equalTo: loginInformationView.leadingAnchor),
             licenseButton.trailingAnchor.constraint(equalTo: loginInformationView.trailingAnchor),
             licenseButton.heightAnchor.constraint(equalTo: dataButton.heightAnchor)
+        ])
+        
+        view.addSubview(withdrawalButton)
+        NSLayoutConstraint.activate([
+            withdrawalButton.topAnchor.constraint(equalTo: licenseButton.bottomAnchor, constant: 30),
+            withdrawalButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            withdrawalButton.widthAnchor.constraint(equalToConstant: 94),
+            withdrawalButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        view.addSubview(versionInformationLabel)
+        NSLayoutConstraint.activate([
+            versionInformationLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -58),
+            versionInformationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 }
