8c949253860ef953e7120f2c7971f9b2e3d6db20
eraser <eraser3031@gmail.com>
2022-11-21 22:52:38 +0900

    :sparkles: 네비게이션바 구현

---
+++ Workade/Views&ViewModels/Explore/ExploreViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ExploreViewController.swift
+//  Workade
+//
+//  Created by 김예훈 on 2022/11/21.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon"), primaryAction: nil)
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: infoButton), UIBarButtonItem(customView: openChatButton)]
+    }
+    
+    lazy var openChatButton: UIButton = {
+        let button = UIButton(type: .custom)
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 14, bottom: 12, trailing: 14)
+        
+        var background = UIButton.Configuration.plain().background
+        background.strokeWidth = 1
+        background.strokeColor = .theme.groupedBackground
+        config.background = background
+        
+        var titleAttr = AttributedString.init("오픈채팅")
+        titleAttr.font = .customFont(for: .caption2)
+        config.attributedTitle = titleAttr
+        
+        config.image = UIImage.fromSystemImage(name: "message.fill",
+                                               font: .systemFont(ofSize: 13, weight: .bold),
+                                               color: .theme.workadeBlue)
+        config.imagePadding = 4
+        button.configuration = config
+        
+        return button
+    }()
+    
+    lazy var infoButton: UIButton = {
+        let button = UIButton(type: .custom)
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 14, bottom: 12, trailing: 14)
+        
+        var background = UIButton.Configuration.plain().background
+        background.backgroundColor = .theme.groupedBackground
+        config.background = background
+        
+        var titleAttr = AttributedString.init("내 정보")
+        titleAttr.font = .customFont(for: .caption2)
+        titleAttr.foregroundColor = .theme.tertiary
+        
+        config.attributedTitle = titleAttr
+        config.image = UIImage.fromSystemImage(name: "person.fill",
+                                               font: .systemFont(ofSize: 13, weight: .bold),
+                                               color: .theme.tertiary)
+        config.imagePadding = 4
+        button.configuration = config
+        
+        return button
+    }()
+    
+}
