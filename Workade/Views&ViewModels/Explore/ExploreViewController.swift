f4c5213c52ef342655e51a3475b46f0d5acc3695
eraser <eraser3031@gmail.com>
2022-11-22 13:07:06 +0900

    :sparkles: RegionInfoView 추가

--- Workade/Views&ViewModels/Explore/ExploreViewController.swift
+++ Workade/Views&ViewModels/Explore/ExploreViewController.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ExploreViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon")?.withRenderingMode(.alwaysOriginal), primaryAction: nil)
         navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: infoButton), UIBarButtonItem(customView: openChatButton)]
         setupLayout()
     }
+    
+    lazy var mainContainerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .theme.sectionBackground
+        view.layer.cornerCurve = .continuous
+        view.layer.cornerRadius = 30
+        view.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    lazy var subContainerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .theme.sectionBackground
+        view.layer.cornerCurve = .continuous
+        view.layer.cornerRadius = 30
+        view.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     
     lazy var openChatButton: UIButton = {
         let button = UIButton(type: .custom)
@@ -85,6 +108,30 @@ class ExploreViewController: UIViewController {
         return imageView
     }()
     
+    lazy var guideButton: UIButton = {
+        let button = UIButton(type: .custom)
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 24, bottom: 16, trailing: 24)
+        
+        var background = UIButton.Configuration.plain().background
+        config.background = background
+        
+        var titleAttr = AttributedString.init("가이드 보러 가기")
+        titleAttr.font = .customFont(for: .caption2)
+        titleAttr.foregroundColor = .theme.tertiary
+        
+        config.attributedTitle = titleAttr
+        config.image = UIImage.fromSystemImage(name: "text.book.closed.fill",
+                                               font: .systemFont(ofSize: 13, weight: .bold),
+                                               color: .theme.tertiary)
+        config.imagePadding = 4
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     private func setupLayout() {
         view.addSubview(titleLabel)
         NSLayoutConstraint.activate([
@@ -94,9 +141,145 @@ class ExploreViewController: UIViewController {
         
         view.addSubview(mapImageView)
         NSLayoutConstraint.activate([
-            mapImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            mapImageView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
             mapImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
             mapImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0)
         ])
+        
+        view.addSubview(guideButton)
+        NSLayoutConstraint.activate([
+            guideButton.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            guideButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+}
+
+class RegionInfoView: UIView {
+    lazy var titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "제주도"
+        label.font = .customFont(for: .title3)
+        label.textColor = .theme.primary
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    lazy var subTitleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "제주특별자치도"
+        label.font = .customFont(for: .caption)
+        label.textColor = .theme.primary
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    lazy var desciptionLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "지금 29명의 사람들이\n워케이션 중이에요"
+        label.textAlignment = .left
+        label.font = .customFont(for: .captionHeadline)
+        label.textColor = .theme.primary
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    lazy var workationButton: UIButton = {
+        let button = UIButton(type: .custom)
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 14, bottom: 12, trailing: 14)
+        
+        var background = UIButton.Configuration.plain().background
+        background.backgroundColor = .theme.workadeBlue
+        config.background = background
+        
+        var titleAttr = AttributedString.init("워케이션 하러 가기")
+        titleAttr.font = .customFont(for: .caption2)
+        titleAttr.foregroundColor = .theme.background
+        
+        config.attributedTitle = titleAttr
+        config.image = UIImage.fromSystemImage(name: "text.book.closed.fill",
+                                               font: .systemFont(ofSize: 13, weight: .bold),
+                                               color: .theme.background)
+        config.imagePadding = 4
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    lazy var dismissButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let image = UIImage.fromSystemImage(name: "xmark", font: .systemFont(ofSize: 15, weight: .bold))?.withRenderingMode(.alwaysOriginal)
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .theme.sectionBackground
+        self.layer.cornerCurve = .continuous
+        self.layer.cornerRadius = 30
+        self.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        
+        let labelStackView = UIStackView(arrangedSubviews: [titleLabel, subTitleLabel])
+        labelStackView.spacing = 0
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .leading
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(labelStackView)
+        NSLayoutConstraint.activate([
+            labelStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            labelStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20)
+        ])
+        
+        self.addSubview(desciptionLabel)
+        NSLayoutConstraint.activate([
+            desciptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            desciptionLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+        self.addSubview(workationButton)
+        NSLayoutConstraint.activate([
+            workationButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            workationButton.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+        
+        self.addSubview(dismissButton)
+        NSLayoutConstraint.activate([
+            dismissButton.widthAnchor.constraint(equalToConstant: 28),
+            dismissButton.heightAnchor.constraint(equalToConstant: 28),
+            dismissButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            dismissButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 20)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct Representable: UIViewRepresentable {
+    func makeUIView(context: Context) -> RegionInfoView {
+        let view = RegionInfoView(frame: .zero)
+        return view
+    }
+    
+    func updateUIView(_ uiView: RegionInfoView, context: Context) { }
+}
+
+struct RegionInfoViewPreview: PreviewProvider {
+    static var previews: some View {
+        Representable()
+            .previewLayout(.fixed(width: 390, height: 150))
+            .background(Color(uiColor: .theme.primary))
     }
 }
