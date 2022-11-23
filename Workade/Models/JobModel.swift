d12481131e5266ff318898ae496d420e0328397d
Inho Choi <eknow25@kakao.com>
2022-11-23 14:51:33 +0900

    :sparkles: Job모델 생성

---
+++ Workade/Models/JobModel.swift
@@ -0,0 +1,21 @@
+//
+//  JobModel.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/11/23.
+//
+
+import Foundation
+
+enum Job: String {
+    case devloper = "개발"
+    case designer = "디자인"
+    case PM = "기획"
+    case marketer = "마케팅"
+    case creater = "콘텐츠 제작"
+    case writer = "작가(글, 웹툰)"
+    case artist = "예술가"
+    case freelancer = "프리랜서"
+    case etc = "기타"
+}
+
