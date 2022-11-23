1b56636292ebec3211f752c06721c688d81214e7
Wonhyuk Choi <whchoi000@gmail.com>
2022-11-24 04:00:49 +0900

    ✨ 스티커 프로그레스뷰 추가

---
+++ Workade/Views&ViewModels/Workation/StickerProgress/StickerProgressViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StickerProgressViewModel.swift
+//  Workade
+//
+//  Created by Wonhyuk Choi on 2022/11/23.
+//
+
+import Foundation
+
+@MainActor
+final class StickerProgressViewModel {
+    
+}
