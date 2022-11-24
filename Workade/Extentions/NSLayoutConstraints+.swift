40e2bdf210660f9208425b66303b4224f39a812d
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-11-24 20:41:55 +0900

    ✨ alpha, layer 등 관련 애니메이션 동작 설정을 편리하게 해주는 확장 정의

--- Workade/Extentions/NSLayoutConstraints+.swift
+++ Workade/Extentions/NSLayoutConstraints+.swift
@@ -9,7 +9,9 @@ import UIKit
 
 extension NSLayoutConstraint {
     /// 기존 NSLayoutConstraint에서 Items만 변경시킨 새로운 NSLayoutConstraint를 반환하는 메서드
-    func replaceItems(item view1: UIView, toItem view2: UIView) -> NSLayoutConstraint {
+    ///
+    /// 추후 사용할 가능성이 있어 넣었습니다. presenting되는 View's 컴포넌트들의 layout을 카피만 하면되서, upstair layout을 작성할 필요가 없어집니다.
+    func replaceItem(item view1: UIView, toItem view2: UIView) -> NSLayoutConstraint {
         let replacedConstraints = NSLayoutConstraint(
             item: view1,
             attribute: firstAttribute, // ex) top, bottom
