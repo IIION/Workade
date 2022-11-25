80cbeba02a124cf0f296af6fa87ba755f813a6a3
ryu <munam4@naver.com>
2022-11-26 02:42:21 +0900

    ♻️  스크롤 시 이미지 alpha값 조정

--- Workade/Views&ViewModels/NearbyPlace/NearbyPlaceDetailView.swift
+++ Workade/Views&ViewModels/NearbyPlace/NearbyPlaceDetailView.swift
@@ -56,7 +56,6 @@ class NearbyPlaceDetailView: UIView {
         setupLayout()
     }
 
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
