09eb9a244a80da727f5aa232e3095e97ab7f375a
Inho Choi <eknow25@kakao.com>
2022-11-28 00:36:20 +0900

    :sparkles: encodable extention 추가

---
+++ Workade/Extentions/Encodable+.swift
@@ -0,0 +1,18 @@
+//
+//  Encodable+.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/11/26.
+//
+
+import Foundation
+
+extension Encodable {
+    /// Object to Dictionary
+    /// cf) Dictionary to Object: JSONDecoder().decode(Object.self, from: dictionary)
+    var asDictionary: [String: Any]? {
+        guard let object = try? JSONEncoder().encode(self),
+              let dictinoary = try? JSONSerialization.jsonObject(with: object, options: []) as? [String: Any] else { return nil }
+        return dictinoary
+    }
+}
