dcf5ca27e760cc647b3e2c7f5b841e21acefa0e1
Inho Choi <eknow25@kakao.com>
2022-11-28 00:35:53 +0900

    :sparkles: User 모델 추가

---
+++ Workade/Models/UserModel.swift
@@ -0,0 +1,45 @@
+//
+//  UserModel.swift
+//  Workade
+//
+//  Created by Inho Choi on 2022/11/26.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let name: String
+    let email: String
+    let job: Job
+}
+
+struct ActiveUser: Codable {
+    let id: String
+    let email: String
+    let region: Region
+}
+
+enum Region: String, Codable {
+    case gyeongGiDo
+    case jeJuDo
+    case gangWonDo
+    case chungCheongNamDo
+    case chungCheongBukDo
+    case jeolLaNamDo
+    case jeolLBbukDo
+    case gyeongSangNamDo
+    case gyeongSangBukDo
+}
+
+enum Job: String, CaseIterable, Codable {
+    case devloper = "개발"
+    case designer = "디자인"
+    case projectManager = "기획"
+    case marketer = "마케팅"
+    case creater = "콘텐츠 제작"
+    case writer = "작가(글, 웹툰)"
+    case artist = "예술가"
+    case freelancer = "프리랜서"
+    case etc = "기타"
+}
