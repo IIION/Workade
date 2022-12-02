cfe36b253f7f19216ef12321c374897c12f0604c
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-12-03 03:45:06 +0900

    ♻️ 불필요한 코드 간결하게 수정

--- Workade/Managers/NetworkMonitor.swift
+++ Workade/Managers/NetworkMonitor.swift
@@ -18,18 +18,13 @@ final class NetworkMonitor {
     
     // 구현 파트
     private var monitor = NWPathMonitor()
-    private var status = NWPath.Status.unsatisfied {
-        didSet {
-            if status == .satisfied {
-                becomeSatisfied.send()
-            }
-        }
-    }
     
     private func startMonitoring() {
         monitor.start(queue: DispatchQueue.global(qos: .background))
         monitor.pathUpdateHandler = { [weak self] path in
-            self?.status = path.status
+            if path.status == .satisfied {
+                self?.becomeSatisfied.send()
+            }
         }
     }
 }
