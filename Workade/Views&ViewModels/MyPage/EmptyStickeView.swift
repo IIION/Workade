46f732d5c449ce022776e09836bf8336bee6823d
jeohong <jeohong59@gmail.com>
2022-12-05 18:02:03 +0900

    ✨ 스티커 비어있을때 표시하는 뷰 파일을 생성

---
+++ Workade/Views&ViewModels/MyPage/EmptyStickeView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyStickeView.swift
+//  Workade
+//
+//  Created by Hong jeongmin on 2022/12/05.
+//
+
+import UIKit
+
+class EmptyStickeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
