b35080ee3fa5aab911ceb554886d5194f245fae2
jeohong <jeohong59@gmail.com>
2022-12-05 18:10:11 +0900

    ✨ 비어있는 스티커 뷰 구현

--- Workade/Views&ViewModels/MyPage/EmptyStickeView.swift
+++ Workade/Views&ViewModels/MyPage/EmptyStickeView.swift
@@ -8,13 +8,46 @@
 import UIKit
 
 class EmptyStickeView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let emptyStickerImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "blank"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let emptyStickerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "워케이션을 해서 스티커를 획득하세요!"
+        label.font = .customFont(for: .captionHeadline)
+        label.textColor = .theme.primary
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        addSubview(emptyStickerImageView)
+        NSLayoutConstraint.activate([
+            emptyStickerImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            emptyStickerImageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        addSubview(emptyStickerLabel)
+        NSLayoutConstraint.activate([
+            emptyStickerLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            emptyStickerLabel.topAnchor.constraint(equalTo: emptyStickerImageView.bottomAnchor, constant: 20)
+        ])
+    }
 }
