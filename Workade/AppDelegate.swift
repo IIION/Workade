a19d3514753722fa7238082096d9bd3366b96319
Wonhyuk Choi <whchoi000@gmail.com>
2022-12-28 22:10:15 +0900

    ♻️ activeUser 쓰기 반복 오류 수정

--- Workade/AppDelegate.swift
+++ Workade/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        NMFAuthManager.shared().clientId = "KEY"
+        NMFAuthManager.shared().clientId = "73fuvr2j6i"
+        KakaoSDK.initSDK(appKey: "beed144107e0a7f9f2886481b17872ff")
         FirebaseApp.configure()
         return true
     }
